3801317101330f0c526e8882727203d877277c0f
Leandro Carrizo <leandro.carrizo@firstdata.com.ar>
2021-04-01 13:12:34 -0300

    Actualizamos catalogo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
 //Initializations
 const app = express();
 
-//Settings
+// Settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views') );
+app.set('views', path.join(__dirname,'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname:
-
+    extname: '.hbs'
 }));
+
 //Middleware
+
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'appSecreta',
+    resave: true,
+    saveUninitialized: true
+}));
 
 //Global Variables
 
